8656f1c849f644de773efc6593d0aca9cd5ddeb7
Max <medmaxim96@gmail.com>
2024-03-08 19:04:22 +0200

    made the filter an extended Kalman and added an explanation

--- kalman/init.m
+++ kalman/init.m
@@ -6,20 +6,13 @@ g = 9.81; % [m/s^2]
 m = 1; % [kg]
 % Pendulum length
 l = 0.5; % [m]
+
 % Sampling time
 Ts = 1/2000; % [s] 
 
-% State space representation
-A = [0 1; -g/l 0];
-B = [0; 1/(m*l^2)];
-C = [1 0];
-D = 0;
+% Put all in a single vector
+params = [g,m,l,Ts];
 
-A = eye(2)+Ts*A;
-B = Ts*B;
-
-% Measurments to take
-H = [1, 0];
 % Process noise covariance
 Q = 1e-5;
 % Measurement noise covariance
