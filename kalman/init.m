1d8659d0663f2d37d9bf87678dfd146f54e084d1
Max <medmaxim96@gmail.com>
2024-03-09 17:18:03 +0200

    Fixed the code in kalman, now it kinda makes more sense

--- kalman/init.m
+++ kalman/init.m
@@ -1,28 +1,31 @@
-% Copyright 2018 The MathWorks, Inc.
-% Pendulum model
-% Gravity
+clear all; close all; clc;
+
+% --- Pendulum model --- %
 g = 9.81; % [m/s^2]
-% Pendulum mass
 m = 1; % [kg]
-% Pendulum length
 l = 0.5; % [m]
 
-% Sampling time
-Ts = 1/2000; % [s] 
-
-% Put all in a single vector
-params = [g,m,l,Ts];
-
-% State space representation (Continues and linearized)
-A = [0 1; -(g/l) 0];
-B = [0; 1/(m*l^2)];
-C = [1 0];
-D = 0;
-
+% --- noise --- %
 % Process noise covariance
 Q = 1e-3;
 % Measurement noise covariance
 R = 1e-4;
+
+% --- Sampling time --- %
+Ts = 1/2000; % [s] 
+
+
+% --- Testing the code against the matlab function --- %
+% Pendulum model
+A = [0 1; -(9.81/0.5) 0];
+B = [0; 1/(1*0.5^2)];
+H = [1 0];
+D = 0;
+% Make the model discrete
+A = eye(2)+Ts*A;
+F = A;
+B = Ts*B;
+
 
 
 
